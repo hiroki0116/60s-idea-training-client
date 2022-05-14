d449026cd8cd011fcf131ee25166dcecadca6287
hirokiseino0116 <hiroki.seino@sparestaff.com.au>
2022-05-14 20:37:06 +1000

    Add font-faminly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+import 'styles/globals.css'
+import CustomNavBar from 'components/Layout/CustomNavBar';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CustomNavBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
