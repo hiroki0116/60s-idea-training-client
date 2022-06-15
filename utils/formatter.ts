29ba57cefb0507caea28bd31ca34e5659c8b8d7b
hiroki0116 <hirokiseino0116@gmail.com>
2022-06-16 00:17:51 +1000

    Add rich text editor on individual record

--- utils/formatter.ts
+++ utils/formatter.ts
@@ -1,5 +1,11 @@
+import { stringify } from "querystring";
+
 export const capitalizeWords = (arr: string[]) => {
     return arr.map((element: string) => {
       return element.charAt(0).toUpperCase() + element.substring(1).toLowerCase();
     });
   }
+
+export const capitalizeFirst = (str:string):string => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
