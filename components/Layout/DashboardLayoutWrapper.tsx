63e27d548f4910b033bd88e7a164adb56d8fe3d0
hiroki0116 <hirokiseino0116@gmail.com>
2022-07-01 22:44:45 +1000

    Change logout logic

--- components/Layout/DashboardLayoutWrapper.tsx
+++ components/Layout/DashboardLayoutWrapper.tsx
@@ -14,11 +14,12 @@ import CenterSpin from './CenterSpin';
 //Components
 import Header from 'components/Layout/Header';
 import Footer from 'components/Layout/Footer';
+import SignUpOrLogin from 'components/auth/SignUpOrLogin';
 
 
 const DashboardLayoutWrapper = ({children}) => {
-    const user: IUser = currAuthUser();
-    const { setUser } = useContext(AuthContext);
+  const user: IUser = currAuthUser();
+  const { setUser } = useContext(AuthContext);
     const customCss = 'text-3xl mb-1';
     const options = [
         {
@@ -40,15 +41,10 @@ const DashboardLayoutWrapper = ({children}) => {
           href: '/settings',
           label: 'Settings',
           icon: <SettingFilled className={customCss} />,
-        },
-        {
-          href: '/logout',
-          label: 'Logout',
-          icon: <LogoutOutlined className={customCss} onClick={()=>handleLogout(setUser)}/>
         }
       ];
     
-      if(!user) return <CenterSpin />
+      if(!user) return <SignUpOrLogin/>
 
   return (
     <div className="grid grid-cols-7 gap-6 p-5 bg-slate-100 min-h-screen">
@@ -56,16 +52,16 @@ const DashboardLayoutWrapper = ({children}) => {
           <div className="place-items-stretch shadow-lg p-2 rounded-lg bg-white">
             <div className="text-center py-2">
               <div>
-                {user.images[0].url ? (
-                  <Avatar src={user.images[0].url} size={60}/>
+                {user?.images[0]?.url ? (
+                  <Avatar src={user?.images[0]?.url} size={60}/>
                 ) : (
                   <Avatar style={{ backgroundColor: '#f15927' }} size={50}>
-                    {user.firstName.charAt(0)}
+                    {user?.firstName?.charAt(0)}
                   </Avatar>
                 )}
               </div>
               <div className="font-bold text-gray-500 text-base mt-2">
-                Welcome, {capitalizeFirst(user.firstName)}!
+                Welcome, {capitalizeFirst(user?.firstName)}!
               </div>
             </div>
           </div>
@@ -73,17 +69,16 @@ const DashboardLayoutWrapper = ({children}) => {
           <div className="grid grid-cols-1 place-items-stretch rounded-lg bg-white shadow-lg mt-6 p-3 text-gray-400 sticky top-10">
             {options.map((option, i) => {
               const style = `w-full rounded-lg transition duration-500 ease-in-out text-gray-500 hover:text-gray-800 hover:bg-blue-50 transform hover:-translate-y-1 mx-auto my-2 p-2 hover:scale-110 ${router.pathname === option.href && 'bg-blue-50 text-gray-800'}`;
-
               return (
                 <div key={i}>
                   {i === 0 && (
                     <div className="text-center text-base py-1 bg-gray-200 rounded-lg mb-2 font-bold ">
-                      <div className="lg:block hidden">Menu</div>
+                      <div className="">Menu</div>
                     </div>
                   )}
                     <div className={style}>
                         <Link href={option.href}>
-                            <button className={`m-auto w-full ${router.pathname === option.href && 'font-bold text-lg text-gray-800'}`}>
+                            <button className={`m-auto w-full ${router.pathname === option.href  && 'font-bold text-lg text-gray-800'}`}>
                                 {option.icon}
                                 <div className='text-sm'>
                                     {option.label}
@@ -94,6 +89,14 @@ const DashboardLayoutWrapper = ({children}) => {
                 </div>
               );
             })}
+            <div className="w-full rounded-lg transition duration-500 ease-in-out text-gray-500 hover:text-gray-800 hover:bg-blue-50 transform hover:-translate-y-1 mx-auto my-2 p-2 hover:scale-110">
+                <button className="m-auto w-full">
+                    <LogoutOutlined className={customCss} onClick={()=>handleLogout(setUser)}/>
+                    <div className='text-sm'>
+                        Logout
+                    </div>
+                </button>
+            </div>
 
           </div>
       </div>
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,7 @@
 export const LOGO_IMAGE = 'https://res.cloudinary.com/sixty-seconds-idea-training-project/image/upload/v1656157908/ApplicationLayout/logo_dd1heiFNU_cbrddf.png';
 export const IDEA_IMAGE = 'https://res.cloudinary.com/sixty-seconds-idea-training-project/image/upload/v1656157908/ApplicationLayout/firstSection_hxlmob.png';
 export const SETTING_BACKGROUND = 'https://res.cloudinary.com/sixty-seconds-idea-training-project/image/upload/v1656157908/ApplicationLayout/background-profile_ficstp.jpg';
+export const DESKTOP_MOCKUP = 'https://res.cloudinary.com/sixty-seconds-idea-training-project/image/upload/v1656679224/ApplicationLayout/smartmockups_l52g0zcm_um3kcs.jpg';
 export const PRIMARY_COLOR = '#0a2339';
 export const CATEGORIES = [ 'Study','Work','News','Finance','Note','Finace', 'Diary', 'Tips','Emotions','Other'];
 export const DEFAULT_CREATED_AT = '1900-01-01T00:00:00.000Z';
