4994bfa0240905448d4a3323004784a36e53fa21
hiroki0116 <hirokiseino0116@gmail.com>
2022-07-03 19:57:26 +1000

    Change font size

--- components/homepage/ThirdSection.tsx
+++ components/homepage/ThirdSection.tsx
@@ -14,7 +14,7 @@ const ThirdSection = () => {
             <div className="col-span-1 self-center justify-self-center block sm:hidden">
                 <Image src={THIRD_SECTION} alt='Man adding data' width={500} height={350} />
             </div>
-            <div className="sm:text-lg text-base font-bold">Simple rules! Easy to continue everyday!</div>
+            <div className="sm:text-lg text-base font-bold pb-5">Simple rules! Brainstorming within 60 seconds! Easy to continue everyday!</div>
 
             <MotionDivWhenView variants = {{visible: { opacity: 1, x: 0 },hidden: { opacity: 0, x: 10 }}}>
                 <div className="grid grid-cols-1 gap-5 pt-5">
@@ -22,7 +22,7 @@ const ThirdSection = () => {
                         <FieldTimeOutlined className='text-6xl'/>
                         <div>
                             <p className="text-xl font-bold">60 seconds only</p>
-                            <p className="text-base leading-5">You have 60 seconds for each session. You will find it challenging to output within 60 seconds.</p>
+                            <p className="sm:text-base text-sm leading-5">You have 60 seconds for each session. You don't have to structure your thoughts! Try to come up with ideas as much as you can in 60 seconds.</p>
                         </div>
                     </div>
                     <div className="flex p-5 gap-8 rounded-lg shadow-lg bg-blue-50 transform transition duration-500 hover:scale-110">
@@ -30,14 +30,14 @@ const ThirdSection = () => {
                         <div>
                             <p className="text-xl font-bold">5 sessions a day</p>
                             <p className="text-base">Idealy, you come up with 4 or 5 ideas per session.</p>
-                            <p className='text-base bg-gray-200 p-1 text-center rounded-lg'>5 sessions * 60 seconds = 5 mins / Day</p>
+                            <p className='sm:text-base text-sm bg-gray-200 p-1 text-center rounded-lg'>5 sessions * 60 seconds = 5 mins / Day</p>
                         </div>
                     </div>
                     <div className="flex p-5 gap-8 rounded-lg shadow-lg bg-blue-50 transform transition duration-500 hover:scale-110">
                         <ReadOutlined className='text-6xl'/>
                         <div>
                             <p className="text-xl font-bold">Make a Note</p>
-                            <p className="text-base leading-5">You can review your sessions and make an organised note for your better ideas.</p>
+                            <p className="sm:text-base text-sm leading-5">You can review your sessions and make an organised note for your better ideas.</p>
                         </div>
                     </div>
                 </div>
