b67ca60192f21e4eef4a178ad14f49ead3ad78a8
hiroki0116 <hirokiseino0116@gmail.com>
2023-03-15 00:52:48 +1100

    Add custom hook for records

---
+++ features/records/hooks/useSubmit.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import { recordsRepository } from "../repositories/repository_records";
+import { IIdeas } from "api-client/models/Ideas";
+import message from "antd/lib/message";
+
+export const useSubmit = ({
+  searchInput,
+  category,
+  createdAtFrom,
+  createdAtTo,
+  current,
+  pageSize,
+  sortByRecent,
+  isLiked,
+}: {
+  searchInput: string;
+  category: string | undefined;
+  createdAtFrom: string;
+  createdAtTo: string;
+  current: number;
+  pageSize: number;
+  sortByRecent: boolean;
+  isLiked: boolean;
+}): {
+  loading: boolean;
+  results: IIdeas[];
+  dataInfo: { totalDocs: number };
+  handleSubmit: () => Promise<void>;
+} => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [results, setResults] = useState<IIdeas[]>();
+  const [dataInfo, setDataInfo] = useState({ totalDocs: 0 });
+
+  const handleSubmit = async () => {
+    try {
+      setLoading(true);
+      const requestBody = {
+        searchInput: searchInput.trim(),
+        category,
+        createdAtFrom,
+        createdAtTo,
+        current,
+        pageSize,
+        sortByRecent,
+        isLiked,
+      };
+
+      const { ideas, totalDocs } = await recordsRepository.searchRecords({
+        requestBody,
+      });
+
+      setResults(ideas);
+      setDataInfo({ totalDocs });
+    } catch (error: any) {
+      console.log(error.message);
+      message.error("Search query is not valid");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    handleSubmit();
+    // eslint-disable-next-line
+  }, [
+    searchInput,
+    current,
+    pageSize,
+    category,
+    createdAtTo,
+    createdAtFrom,
+    sortByRecent,
+    isLiked,
+  ]);
+
+  return { results, dataInfo, loading, handleSubmit };
+};
