28cf176e37b98fcb829c4944162cc90d47d4defe
hiroki0116 <hirokiseino0116@gmail.com>
2023-03-17 16:46:21 +1100

    Fix error page path

--- api-client/api-client.ts
+++ api-client/api-client.ts
@@ -48,7 +48,7 @@ const errorResponseHandler = (error: any) => {
     } else if (status === 500) {
       return Router.push("/error/500");
     } else if (status === 401 || status == 403) {
-      return Router.push("/403");
+      return Router.push("/error/403");
     } else {
       console.error(error.response.data.message);
       return Promise.reject(error);
